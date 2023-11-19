cf14f66eea0a9937448181e4b9ca894c31a17d0e
domibron <98326467+domibron@users.noreply.github.com>
2023-11-19 22:55:32 +0000

    some things not really started but started

---
+++ Cyber Vault/Assets/Scripts/Interfaces/IWASDInput.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CyberVault.Input
+{
+    public interface IWASDInput
+    {
+        public bool W_Key_Hold();
+        public bool A_Key_Hold();
+        public bool S_Key_Hold();
+        public bool D_Key_Hold();
+    }
+}
